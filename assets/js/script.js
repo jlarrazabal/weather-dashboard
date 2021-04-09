5fcd1867a3f67861f89899893c19a0b000f2cb24
jlarrazabal <69408299+jlarrazabal@users.noreply.github.com>
2021-04-09 00:40:38 -0400

    Minor adjustments to index.html and styles.css. Script.js: work in progress; functions to render the current weather and the 5 days forecast added; pending to resolve the issue about rendering the weather when clicking on a city from the dynamic list.
    - Minor adjustments to index.html and styles.css.
    - Script.js: work in progress; functions to render the current weather and the 5 days forecast added; pending to resolve the issue about rendering the weather when clicking on a city from the dynamic list.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,31 @@ const defaultCity = "Miami";
 const cities = $("#cities");
 const cityLi = $(".list-group-item");
 var cityName = "";
+var cityNames = [];
+var storedCityNames;
 
-
+var init = function() {
+  if (localStorage.getItem("searched-cities") !== null) {
+    cityNames = JSON.parse(localStorage.getItem("searched-cities"));
+    for (var i = 0; i < cityNames.length; i++) {
+      let newCity = $("<li>");
+      newCity.text(cityNames[i]);
+      newCity.addClass("list-group-item");
+      cities.append(newCity);
+    }
+    // addLiListener();
+    getCurrentWeather(defaultCity);
+  } else {
+    let newCity = $("<li>");
+    newCity.text(defaultCity);
+    newCity.addClass("list-group-item");
+    cities.append(newCity);
+    cityNames.push(defaultCity);
+    localStorage.setItem("searched-cities", JSON.stringify(cityNames));
+    // addLiListener();
+    getCurrentWeather(defaultCity);
+  }
+}
 
 var addCityToList = function(city) {
   console.log(city);
@@ -23,7 +46,10 @@ var addCityToList = function(city) {
     newCity.text(requestedCity);
     newCity.addClass("list-group-item");
     cities.append(newCity);
+    // addLiListener();
     getCurrentWeather(requestedCity);
+    cityNames.push(requestedCity);
+    localStorage.setItem("searched-cities", JSON.stringify(cityNames));
     $(city.target).prev().val("");
   }
 }
@@ -37,8 +63,8 @@ var renderKnownCity = function(city) {
 
 var getCurrentWeather = function(city) {
   let currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
-  fetch(currentWeatherUrl).then(function(response){
-    if(!response.ok){
+  fetch(currentWeatherUrl).then(function(response) {
+    if (!response.ok) {
       $(function() {
         $("#error").dialog();
       });
@@ -47,17 +73,80 @@ var getCurrentWeather = function(city) {
     } else {
       return response.json();
     }
-  }).then(function(data){
+  }).then(function(data) {
     console.log(data);
+    let cityNameReturned = data.name;
+    let temp = data.main.temp + " °F";
+    let humidity = data.main.humidity + "%";
+    let windSpeed = data.wind.speed + " miles/hour"
+    let lon = data.coord.lon;
+    let lat = data.coord.lat;
+    let clouds = data.weather[0].icon;
+    let cloudsUrl = "http://openweathermap.org/img/wn/" + clouds + "@2x.png";
+    getUVinformation(lon, lat);
+    getFiveDaysForecast(lon, lat);
+    console.log(temp);
+    console.log(humidity);
+    console.log(windSpeed);
+    console.log(clouds);
+    console.log(cloudsUrl);
+    console.log(lon);
+    console.log(lat);
+    $("#city-selected").text(cityNameReturned);
+    $("#weather-icon").attr("src", cloudsUrl);
+    $("#temp").text(temp);
+    $("#humidity").text(humidity);
+    $("#wind-speed").text(windSpeed);
   });
 }
 
+var getUVinformation = function(lon, lat) {
+  let uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+  fetch(uvURL).then(function(response) {
+    if (!response.ok) {
+      $(function() {
+        $("#error").dialog();
+      });
+      $(".list-group-item").last().remove();
+      console.log(response);
+    } else {
+      return response.json();
+    }
+  }).then(function(data) {
+    console.log(data);
+    let uvi = data.current.uvi;
+    console.log(uvi);
+    $("#uv-index").text(uvi);
+  });
+}
 
-
-
-
-
-
+var getFiveDaysForecast = function(lon, lat) {
+  let uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+  fetch(uvURL).then(function(response) {
+    if (!response.ok) {
+      $(function() {
+        $("#error").dialog();
+      });
+      $(".list-group-item").last().remove();
+      console.log(response);
+    } else {
+      return response.json();
+    }
+  }).then(function(data) {
+    console.log(data);
+    for (var i = 0; i < 5; i++) {
+      let day = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
+      let temp = data.daily[i].temp.day + " °F";
+      let humidity = data.daily[i].humidity + "%";
+      let clouds = data.daily[i].weather[0].icon;
+      let cloudsUrl = "http://openweathermap.org/img/wn/" + clouds + "@2x.png";
+      $("#day" + i).text(day);
+      $("#img" + i).attr("src", cloudsUrl);
+      $("#temp" + i).text(temp);
+      $("#humidity" + i).text(humidity);
+    }
+  });
+}
 
 searchForm.on("click", "#search-btn", function(event) {
   event.preventDefault();
@@ -68,6 +157,10 @@ searchForm.on("submit", function(event) {
   event.preventDefault();
 });
 
-cityLi.on("click", function(event) {
-  renderKnownCity(event)
-});
+// var addLiListener = function() {
+//   document.querySelectorAll(".list-group-item").addEventListener("click", function(event) {
+//     renderKnownCity(event);
+//   });
+// }
+
+init();
